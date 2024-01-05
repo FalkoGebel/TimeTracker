6b27ebc8919ea50b52fc2a2ca7c5e82b0114872d
Falko Gebel <63905869+FalkoGebel@users.noreply.github.com>
2024-01-05 18:00:19 +0100

    added library resources, started logic implementation and updated tests

---
+++ TrackerLibrary/TrackerLogic.cs
@@ -0,0 +1,20 @@
+﻿namespace TrackerLibrary
+{
+    public static class TrackerLogic
+    {
+        public static TrackedTimeModel Start(string categoryName)
+        {
+            if (categoryName == string.Empty)
+                throw new ArgumentException(Properties.Resources.ERROR_NO_CATEGORY_NAME);
+
+            return new TrackedTimeModel()
+            {
+                Category = new CategoryModel()
+                {
+                    Name = categoryName
+                },
+                Start = DateTime.Now
+            };
+        }
+    }
+}
---
+++ TrackerLibraryTests/TrackerLogicTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using System.Globalization;
+using TrackerLibrary;
+
+namespace TrackerLibraryTests
+{
+    [TestClass]
+    public class TrackerLogicTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+        }
+
+        [TestMethod]
+        public void Call_Start_without_Category_and_exception()
+        {
+            Action act = () => TrackerLogic.Start("");
+
+            act.Should().Throw<ArgumentException>().WithMessage("No category name specified");
+        }
+
+        [TestMethod]
+        public void Call_Start_without_Category_and_exception_in_german()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+
+            Action act = () => TrackerLogic.Start("");
+
+            act.Should().Throw<ArgumentException>().WithMessage("Kein Kategorienname vorgegeben");
+        }
+
+        [TestMethod]
+        public void Call_Start_with_Category_and_result_is_valid()
+        {
+            string categoryName = "category";
+            
+            TrackedTimeModel sut = TrackerLogic.Start(categoryName);
+
+            sut.Should().NotBeNull();
+            sut.Category.Should().NotBeNull();
+            sut.Start.Should().BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 1));
+            sut.End.Should().Be(DateTime.MinValue);
+            sut.Category.Name.Should().NotBeNull();
+            sut.Category.Name.Should().Be(categoryName);
+        }
+    }
+}
