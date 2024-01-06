e5ebcd851054c730bddc123dd09fb9be15dc898c
Falko Gebel <63905869+FalkoGebel@users.noreply.github.com>
2024-01-06 16:20:51 +0100

    extended functionality

--- TrackerLibrary/TrackerLogic.cs
+++ TrackerLibrary/TrackerLogic.cs
@@ -2,19 +2,64 @@
 {
     public static class TrackerLogic
     {
+        private static readonly List<CategoryModel> categories = [];
+        private static readonly List<TrackedTimeModel> trackedTimeModels = [];
+
+        public static void AddCategory(string categoryName)
+        {
+            categories.Add(new CategoryModel()
+            {
+                Name = categoryName,
+                Active = true
+            });
+        }
+
+        public static bool CategoryExists(string categoryName)
+        {
+            return categories.Find(c => c.Name == categoryName) != null;
+        }
+
+        public static List<TrackedTimeModel> GetTrackedTimeModels()
+        {
+            return trackedTimeModels;
+        }
+
+        public static void RemoveCategory(string v)
+        {
+            throw new NotImplementedException();
+        }
+
         public static TrackedTimeModel Start(string categoryName)
         {
             if (categoryName == string.Empty)
-                throw new ArgumentException(Properties.Resources.ERROR_NO_CATEGORY_NAME);
+                throw new ArgumentException(Properties.Resources.ERR_CATEGORY_NAME);
+
+            CategoryModel categoryModel = categories.Find(c => c.Name == categoryName)
+                ?? throw new ArgumentException(Properties.Resources.ERR_CATEGORY_NOT_FOUND.Replace("{category_name}", categoryName));
+            
+            if (!categoryModel.Active)
+                throw new ArgumentException(Properties.Resources.ERR_CATEGORY_NOT_ACTIVE.Replace("{category_name}", categoryName));
 
             return new TrackedTimeModel()
-            {
-                Category = new CategoryModel()
-                {
-                    Name = categoryName
-                },
+            { 
+                Category = categoryModel,
                 Start = DateTime.Now
             };
+        }
+
+        public static void Stop(TrackedTimeModel trackedTimeModel)
+        {
+            if (trackedTimeModel == null)
+                throw new ArgumentException(Properties.Resources.ERR_TTM);
+
+            if (trackedTimeModel.Category == null)
+                throw new ArgumentException(Properties.Resources.ERR_TTM_CM);
+
+            if (trackedTimeModel.Start == DateTime.MinValue)
+                throw new ArgumentException(Properties.Resources.ERR_TTM_START);
+
+            trackedTimeModel.End = DateTime.Now;
+            trackedTimeModels.Add(trackedTimeModel);
         }
     }
 }
--- TrackerLibraryTests/TrackerLogicTests.cs
+++ TrackerLibraryTests/TrackerLogicTests.cs
@@ -32,10 +32,24 @@ namespace TrackerLibraryTests
         }
 
         [TestMethod]
+        public void Call_CategoryExists_with_invalid_category_and_return_false()
+        {
+            TrackerLogic.CategoryExists("Reading").Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Call_AddCategory_with_new_category_and_category_exists()
+        {
+            TrackerLogic.AddCategory("Writing");
+            TrackerLogic.CategoryExists("Writing").Should().BeTrue();
+        }
+
+        [TestMethod]
         public void Call_Start_with_Category_and_result_is_valid()
         {
-            string categoryName = "category";
-            
+            string categoryName = "Programming";
+
+            TrackerLogic.AddCategory(categoryName);
             TrackedTimeModel sut = TrackerLogic.Start(categoryName);
 
             sut.Should().NotBeNull();
@@ -44,6 +58,106 @@ namespace TrackerLibraryTests
             sut.End.Should().Be(DateTime.MinValue);
             sut.Category.Name.Should().NotBeNull();
             sut.Category.Name.Should().Be(categoryName);
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_TrackedTimeModel_and_exception()
+        {
+            TrackedTimeModel sut = null;
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("TrackedTimeModel must not be \"null\"");
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_TrackedTimeModel_and_exception_in_german()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+
+            TrackedTimeModel sut = null;
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("TrackedTimeModel darf nicht \"null\" sein");
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_Category_and_exception()
+        {
+            TrackedTimeModel sut = new ()
+            {
+                Category = null
+            };
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("Category in TrackedTimeModel must not be \"null\"");
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_Category_and_exception_in_german()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+
+            TrackedTimeModel sut = new()
+            {
+                Category = null
+            };
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("Category in TrackedTimeModel darf nicht \"null\" sein");
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_Start_and_exception()
+        {
+            CategoryModel categoryModel = new()
+            {
+                Name = "category"
+            };
+            TrackedTimeModel sut = new()
+            {
+                Category = categoryModel
+            };
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("No Start specified in TrackedTimeModel");
+        }
+
+        [TestMethod]
+        public void Call_Stop_without_Start_and_exception_in_german()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+
+            CategoryModel categoryModel = new()
+            {
+                Name = "category"
+            };
+            TrackedTimeModel sut = new()
+            {
+                Category = categoryModel
+            };
+            Action act = () => TrackerLogic.Stop(sut);
+
+            act.Should().Throw<ArgumentException>().WithMessage("Kein Start in TrackedTimeModel spezifiziert");
+        }
+
+        [TestMethod]
+        public void Call_Stop_wit_correct_data_and_new_TrackedTimeModel_with_Stop_in_list()
+        {
+            CategoryModel categoryModel = new()
+            {
+                Name = "category"
+            };
+            DateTime start = DateTime.Now - new TimeSpan(0, 10, 30);
+            TrackedTimeModel inputTrackedTimeModel = new()
+            {
+                Category = categoryModel,
+                Start = start
+            };
+            TrackerLogic.Stop(inputTrackedTimeModel);
+            TrackedTimeModel sut = TrackerLogic.GetTrackedTimeModels().Last();
+            sut.Category.Should().Be(categoryModel);
+            sut.Start.Should().Be(start);
+            sut.End.Should().BeCloseTo(DateTime.Now, new TimeSpan(0, 0, 1));
         }
     }
 }
